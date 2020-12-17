3e91bf9539b10e0017e9636e1e8f75eeb5903dd9
GAPPPE <yossapol_01@hotmail.com>
2020-12-17 11:16:41 +0700

    new upload

--- GapysApp/gapys_app/lib/main.dart
+++ GapysApp/gapys_app/lib/main.dart
@@ -1,4 +1,82 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+
+
+class covidToday{
+  static Future<todayCase> covid() async
+  {
+    var url = "https://covid19.th-stat.com/api/open/today";
+    var response = await http.get(url);
+    Map map=json.decode(response.body);
+    todayCase today = todayCase.fromJson(map);
+    return today;
+  }
+}
+class todayCase {
+  int confirmed;
+  int recovered;
+  int hospitalized;
+  int deaths;
+  int newConfirmed;
+  int newRecovered;
+  int newHospitalized;
+  int newDeaths;
+  String updateDate;
+  String source;
+  String devBy;
+  String severBy;
+
+  todayCase(
+      {
+        this.confirmed,
+        this.recovered,
+        this.hospitalized,
+        this.deaths,
+        this.newConfirmed,
+        this.newRecovered,
+        this.newHospitalized,
+        this.newDeaths,
+        this.updateDate,
+        this.source,
+        this.devBy,
+        this.severBy});
+
+  todayCase.fromJson(Map<String, dynamic> json) {
+    confirmed = json['Confirmed'];
+    recovered = json['Recovered'];
+    hospitalized = json['Hospitalized'];
+    deaths = json['Deaths'];
+    newConfirmed = json['NewConfirmed'];
+    newRecovered = json['NewRecovered'];
+    newHospitalized = json['NewHospitalized'];
+    newDeaths = json['NewDeaths'];
+    updateDate = json['UpdateDate'];
+    source = json['Source'];
+    devBy = json['DevBy'];
+    severBy = json['SeverBy'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['Confirmed'] = this.confirmed;
+    data['Recovered'] = this.recovered;
+    data['Hospitalized'] = this.hospitalized;
+    data['Deaths'] = this.deaths;
+    data['NewConfirmed'] = this.newConfirmed;
+    data['NewRecovered'] = this.newRecovered;
+    data['NewHospitalized'] = this.newHospitalized;
+    data['NewDeaths'] = this.newDeaths;
+    data['UpdateDate'] = this.updateDate;
+    data['Source'] = this.source;
+    data['DevBy'] = this.devBy;
+    data['SeverBy'] = this.severBy;
+    return data;
+  }
+}
 
 void main() {
   runApp(Gappe());
@@ -7,190 +85,349 @@ void main() {
 class Gappe extends StatelessWidget {
   var _title ="GapysApp";
 
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(title: "_title",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text(_title),
 
+      home: Scaffold(
+
+        appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50),
+
+          child:   AppBar(
+            elevation: 5,
+            title: Text(_title),
+            backgroundColor: Colors.orange,
+          ),
+        ),
+        drawer: drawerSections,
+        body: ListView(children: <Widget>[
+          Row(
+             crossAxisAlignment: CrossAxisAlignment.center,
+             mainAxisSize: MainAxisSize.max,
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: <Widget>[
+               Container(
+                 width: 200,
+                 height: 50,
+                 child: Card(
+                   child: Center(child:Text('รายงานสถานการณ์วันนี้',style: TextStyle(fontWeight: FontWeight.bold), ) ),
+                 ),
+               )
+            ]
+        ),
+          headerSections,dateData(),
+        ],
       ),
-      drawer: drawerSections,
-      body: ListView(children: <Widget>[
-        headerSections,titleSections,buttonSections,courseSections,rowSections
-      ],),
-    )
-      ,);
+    ),);
   }
 }
 
-Widget headerSections =
+Drawer drawerSections = Drawer(
+  child: ListView(
 
-   Image.network("https://miro.medium.com/max/11400/1*lS9ZqdEGZrRiTcL1JUgt9w.jpeg"
-);
-Widget titleSections = Padding(padding: EdgeInsets.all(30),
-
-    child : Row(
-      children: <Widget>[
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text("Gappe",
-                  style: TextStyle(fontWeight: FontWeight.bold),),
-
-              Text("Yodsaphon",
-                  style: TextStyle(color: Colors.grey[500]),),
-              FavoriteWidget()
-
-
-            ],
-          ),
+    padding: EdgeInsets.zero,
+    children: <Widget>[
+      DrawerHeader(
+        child: Text("Menu",
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
-
-
-
-
-    ],
-  )
-);
-
-
-int Count = 0;
-Widget buttonSections = Row(
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: <Widget>[
-    _buttonColumn(icon: Icons.favorite ,label: "Love"),
-    _buttonColumn(icon: Icons.comment ,label: "Comment"),
-    _buttonColumn(icon: Icons.share ,label: "Share"),
-    IconButton(icon: Icon(Icons.favorite),
-    onPressed: (){
-
-      setState(){
-        Count+=1;
-      }
-        print("click");
-      setState();
-    },),
-      Text('$Count')
-  ],
-);
-
-
-Column _buttonColumn({IconData icon,String label})
-{
-  var icColor = Colors.grey[500];
-  return Column(
-
-    children: <Widget>[
-      Icon(icon,
-        color: icColor,),
-      Container(
-        margin: EdgeInsets.all(10),
-        child: Text(label,
-          style:TextStyle(
-              color: icColor,
-              fontWeight: FontWeight.bold),
+        curve: Curves.linearToEaseOut,
+        decoration: BoxDecoration(
+            color: Colors.blueGrey
         ),
       ),
+      ListTile(
+
+        title: IconButton(
+          icon: Icon(Icons.today),
+          onPressed: (){
+
+          },
+        ),
 
 
+      ),
+      ListTile(
+        title: Text("sections 2"),
+        onTap:(){
+
+
+        },
+      )
     ],
-  );
-}
-Widget courseSections = Container(
+  ),
 
-  height: 600,color: Colors.deepOrangeAccent,
 
 );
 
-Widget rowSections = Stack(
+
+
+Widget headerSections =Column(
+    mainAxisAlignment: MainAxisAlignment.center,
   children: <Widget>[
-    Row(
-      children: <Widget>[
-        Container(
-          margin: EdgeInsets.all(80),
-          color: Colors.deepOrangeAccent,
-          child: Text("test1"),
-        ) ,
-        Text("test2")
-      ],
-    )
+      newConfirm(),
+      deathConfirm(),
+      totalCase(),
+      recover(),
+      hospitalStay(),
+
+
   ],
 );
 
 
 
-Drawer drawerSections = Drawer(
-    child: ListView(
-
-      padding: EdgeInsets.zero,
-      children: <Widget>[
-        DrawerHeader(
-          child: Text("Menu",
-          style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          curve: Curves.linearToEaseOut,
-          decoration: BoxDecoration(
-              color: Colors.blueGrey
-          ),
-        ),
-        ListTile(
-          title: Text("sections 1"),
-          onTap: (){
-          },
-        ),
-        ListTile(
-          title: Text("sections 2"),
-          onTap:(){
-
-          },
-        )
-      ],
-    ),
-
-
-);
 
 
 
-// love button
-class FavoriteWidget extends StatefulWidget {
+
+class newConfirm extends StatefulWidget {
   @override
-  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+  _newConfirmState createState() => _newConfirmState();
 }
 
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorited = true ;
-  int  _favoriteCount = 1;
-
+class _newConfirmState extends State<newConfirm> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        IconButton(
-          icon: _isFavorited?Icon(Icons.favorite):Icon(Icons.favorite_border),
+   return Container(
+          width: 200,
+          height: 100,
 
-          color: Colors.red,
-          onPressed: (){
-            setState(() {
-              if (_isFavorited) {
-                _favoriteCount -=1;
-                _isFavorited = false;
 
-              }
-              else {
-                _favoriteCount +=1;
-                _isFavorited = true;
-              }
-            });},
-        ),
-        Container(
-          margin: EdgeInsets.only(left: 8),
-          child: Text('$_favoriteCount',
-            style: TextStyle(color: Colors.grey),),
+          child: Card(
+              child:Column(
+                children: <Widget>[
+                  FutureBuilder(
+                    future: covidToday.covid(),
+                    builder: (context,covid){
+                      if(covid.hasData){
+                        todayCase msg = covid.data;
+                        var newconfirm = msg.newConfirmed;
+                        // ignore: missing_return, missing_return
+                        return Center(child: Text('ผู้ป่วยเพิ่มมาใหม่ : ${newconfirm}',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),) ;
+                      }
+                      else
+                        return Text("wait");
+                    },
+                  ),
+                  Icon(Icons.accessibility_new,size: 50,color: Colors.red,)
+
+                ],
+              )
+
+
+          ),
+        );
+
+  }
+  
+}
+
+class dateData extends StatefulWidget {
+  @override
+  _dateDataState createState() => _dateDataState();
+}
+
+class _dateDataState extends State<dateData> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Card(
+        child:Column(
+          children: <Widget>[
+            FutureBuilder(
+              future: covidToday.covid(),
+              builder: (context,covid){
+                if(covid.hasData){
+                  todayCase msg = covid.data;
+                  print(msg.toJson());
+                  var date= msg.updateDate;
+                  var source = msg.source;
+                  // ignore: missing_return, missing_return
+                  return Center(child: Text('ข้อมูลวันที่ : ${date}\nข้อมูลจาก ${source}',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),) ;
+                }
+                else
+                  return Text("wait");
+              },
+            ),
+
+
+          ],
         )
-      ],
+
+
+    ),);
+  }
+}
+
+
+class deathConfirm extends StatefulWidget {
+  @override
+  _deathConfirmState createState() => _deathConfirmState();
+}
+
+class _deathConfirmState extends State<deathConfirm> {
+  @override
+  Widget build(BuildContext context) {
+    return
+        Container(
+
+          width: 200,
+          height: 100,
+
+          child: Card(
+              child:Column(
+                children: <Widget>[
+                  FutureBuilder(
+                    future: covidToday.covid(),
+                    builder: (context,covid){
+                      if(covid.hasData){
+                        todayCase msg = covid.data;
+                        int deathcf = msg.newDeaths;
+                        int death   = msg.deaths;
+                        // ignore: missing_return, missing_return
+                        return Center(child: Text('ผู้เสียชีวิตตวันนี้ : ${deathcf}\nยอดผู้เสียชีวิต ${death}',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),) ;
+                      }
+                      else
+                        return Text("wait");
+                    },
+                  ),
+                  Icon(Icons.airline_seat_flat,size: 50,color: Colors.red,)
+
+                ],
+              )
+
+
+          ),
+        );
+
+}
+}
+
+class totalCase extends StatefulWidget {
+  @override
+  _totalCaseState createState() => _totalCaseState();
+}
+
+class _totalCaseState extends State<totalCase> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 100,
+
+      child: Card(
+
+          child:Column(
+            children: <Widget>[
+              FutureBuilder(
+                future: covidToday.covid(),
+                builder: (context,covid){
+                  if(covid.hasData){
+                    todayCase msg = covid.data;
+                    int confirm = msg.confirmed;
+
+                    return Center(child: Text('ทั้งหมด : ${confirm}',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),) ;
+                  }
+                  else
+                    return Text("wait");
+                },
+              ),
+              Icon(Icons.done_all,size: 50,color: Colors.red,)
+
+            ],
+          )
+
+
+      ),
     );
   }
 }
+// ignore: camel_case_types
+class recover extends StatefulWidget {
+  @override
+  _recoverState createState() => _recoverState();
+}
+
+class _recoverState extends State<recover> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 100,
+
+      child: Card(
+          child:Column(
+            children: <Widget>[
+              FutureBuilder(
+                future: covidToday.covid(),
+                builder: (context,covid){
+                  if(covid.hasData){
+                    todayCase msg = covid.data;
+                    int recovered = msg.recovered;
+                    // ignore: missing_return, missing_return
+                    return Center(child: Text('หายแล้ว : ${recovered}',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),) ;
+                  }
+                  else
+                    return Text("wait");
+                },
+              ),
+              Icon(Icons.local_hospital,size: 50,color: Colors.red,)
+
+            ],
+          )
+
+
+      ),
+    );
+  }
+}
+
+
+class hospitalStay extends StatefulWidget {
+  @override
+  _hospitalStayState createState() => _hospitalStayState();
+}
+
+class _hospitalStayState extends State<hospitalStay> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 100,
+
+      child: Card(
+          child:Column(
+            children: <Widget>[
+              FutureBuilder(
+                future: covidToday.covid(),
+                builder: (context,covid){
+                  if(covid.hasData){
+                    todayCase msg = covid.data;
+                    int hospital = msg.hospitalized;
+                    int recovertoday = msg.newRecovered;
+                    // ignore: missing_return, missing_return
+                    return Center(child: Text('กำลังรักษา ${hospital}\n หายวันนี้ ${recovertoday}',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),) ;
+                  }
+                  else
+                    return Text("wait");
+                },
+              ),
+              Icon(Icons.favorite,size: 50,color: Colors.red,)
+            ],
+          )
+
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
